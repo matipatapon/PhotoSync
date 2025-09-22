367892be1bf8e1d680003649726529b9de90a341
Narehate <matipietka@gmail.com>
2025-09-22 17:29:51 +0200

    [SERVER] Added thumbnail handling to FileDataEndpoint

--- server/src/endpoint/file_data_endpoint_test.go
+++ server/src/endpoint/file_data_endpoint_test.go
@@ -2,6 +2,7 @@ package endpoint_test
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -17,19 +18,20 @@ import (
 	"testing"
 )
 
-var FILE_DATA_QUERY string = "SELECT id, filename, TO_CHAR(creation_date, 'YYYY.MM.DD HH24:MI:SS'), mime_type, size FROM files WHERE user_id = $1 ORDER BY creation_date DESC, id DESC LIMIT $2 OFFSET $3"
+var FILE_DATA_QUERY string = "SELECT id, filename, TO_CHAR(creation_date, 'YYYY.MM.DD HH24:MI:SS'), mime_type, size, thumbnail FROM files WHERE user_id = $1 ORDER BY creation_date DESC, id DESC LIMIT $2 OFFSET $3"
 var OFFSET int64 = 10
 var COUNT int64 = 15
 var NEGATIVE_OFFSET int64 = -10
 var NEGATIVE_COUNT int64 = -15
 
-var FILE_DATA []endpoint.FileData = []endpoint.FileData{
+var FILE_DATA_WITH_THUMBNAIL []endpoint.FileData = []endpoint.FileData{
 	{
 		Id:           "13",
 		Filename:     "B2.jpg",
 		CreationDate: "2023.01.04 12:30:00",
 		MIMEType:     "image/jpeg",
 		Size:         "1024",
+		Thumbnail:    "TU9SVEFERUxLQQ==",
 	},
 	{
 		Id:           "17",
@@ -37,21 +39,41 @@ var FILE_DATA []endpoint.FileData = []endpoint.FileData{
 		CreationDate: "2022.04.06 14:33:12",
 		MIMEType:     "image/jpeg",
 		Size:         "5000",
+		Thumbnail:    "TFVORUNaS0E=",
+	},
+}
+
+var FILE_DATA_WITHOUT_THUMBNAIL []endpoint.FileData = []endpoint.FileData{
+	{
+		Id:           "13",
+		Filename:     "B2.jpg",
+		CreationDate: "2023.01.04 12:30:00",
+		MIMEType:     "image/jpeg",
+		Size:         "1024",
+		Thumbnail:    "",
 	},
 }
 
 func fileDataToRequestArgs(fileData []endpoint.FileData) [][]any {
 	result := [][]any{}
 	for _, fd := range fileData {
+		row := []any{}
 		id, _ := strconv.ParseInt(fd.Id, 10, 64)
+		row = append(row, id)
+		row = append(row, fd.Filename)
+		row = append(row, fd.CreationDate)
+		row = append(row, int16(metadata.StringToMIMEType(fd.MIMEType)))
 		size, _ := strconv.ParseInt(fd.Size, 10, 64)
-		result = append(result, []any{
-			id,
-			fd.Filename,
-			fd.CreationDate,
-			int16(metadata.StringToMIMEType(fd.MIMEType)),
-			size,
-		})
+		row = append(row, size)
+
+		if fd.Thumbnail == "" {
+			row = append(row, nil)
+		} else {
+			thumbnail, _ := base64.StdEncoding.DecodeString(fd.Thumbnail)
+			row = append(row, thumbnail)
+		}
+
+		result = append(result, row)
 	}
 	return result
 }
@@ -118,7 +140,7 @@ func TestFileDataEndpointShouldReturnProperHeadersDuringPreflight(t *testing.T) 
 
 func TestFileDataEndpointShouldReturn500WhenQueryFailed(t *testing.T) {
 	databaseMock := mock.NewDatabaseMock(t)
-	databaseMock.ExpectQuery(FILE_DATA_QUERY, fileDataToRequestArgs(FILE_DATA), []any{USER_ID, int64(15), int64(10)}, errors.New("db error"))
+	databaseMock.ExpectQuery(FILE_DATA_QUERY, fileDataToRequestArgs(FILE_DATA_WITH_THUMBNAIL), []any{USER_ID, int64(15), int64(10)}, errors.New("db error"))
 	defer databaseMock.AssertAllExpectionsSatisfied()
 
 	jwtManagerMock := mock.NewJwtManagerMock(t)
@@ -247,9 +269,105 @@ func TestFileDataEndpointShouldReturn400WhenOffsetIsMissing(t *testing.T) {
 	}
 }
 
+func TestFileDataEndpointShouldReturn500WhenFileGotDeleted(t *testing.T) {
+	results := [][][]any{{{}}, {}}
+	for _, result := range results {
+		fileId, _ := strconv.ParseInt(FILE_DATA_WITHOUT_THUMBNAIL[0].Id, 10, 64)
+
+		databaseMock := mock.NewDatabaseMock(t)
+		databaseMock.ExpectQuery(FILE_DATA_QUERY, fileDataToRequestArgs(FILE_DATA_WITHOUT_THUMBNAIL), []any{USER_ID, int64(15), int64(10)}, nil)
+		databaseMock.ExpectQuery("SELECT file FROM files WHERE id = $1", result, []any{fileId}, nil)
+		defer databaseMock.AssertAllExpectionsSatisfied()
+
+		jwtManagerMock := mock.NewJwtManagerMock(t)
+		jwtManagerMock.ExpectDecode(TOKEN_STRING, jwt.JwtPayload{
+			UserId: USER_ID,
+		}, nil)
+		defer jwtManagerMock.AssertAllExpectionsSatisfied()
+
+		sut := endpoint.NewFileDataEndpoint(&databaseMock, &jwtManagerMock)
+		router, responseRecorder := prepareGin()
+		router.GET("/", sut.Get)
+
+		router.ServeHTTP(responseRecorder, prepareRequest(&OFFSET, &COUNT))
+
+		if responseRecorder.Code != 500 {
+			t.Error(responseRecorder.Code)
+		}
+		if responseRecorder.Body.Len() != 0 {
+			t.Error("Expected empty response")
+		}
+	}
+}
+
+func TestFileDataEndpointShouldReturn500WhenFailedToGetAImage(t *testing.T) {
+	fileId, _ := strconv.ParseInt(FILE_DATA_WITHOUT_THUMBNAIL[0].Id, 10, 64)
+
+	databaseMock := mock.NewDatabaseMock(t)
+	databaseMock.ExpectQuery(FILE_DATA_QUERY, fileDataToRequestArgs(FILE_DATA_WITHOUT_THUMBNAIL), []any{USER_ID, int64(15), int64(10)}, nil)
+	databaseMock.ExpectQuery("SELECT file FROM files WHERE id = $1", [][]any{{}}, []any{fileId}, errors.New("failed to get a file"))
+	defer databaseMock.AssertAllExpectionsSatisfied()
+
+	jwtManagerMock := mock.NewJwtManagerMock(t)
+	jwtManagerMock.ExpectDecode(TOKEN_STRING, jwt.JwtPayload{
+		UserId: USER_ID,
+	}, nil)
+	defer jwtManagerMock.AssertAllExpectionsSatisfied()
+
+	sut := endpoint.NewFileDataEndpoint(&databaseMock, &jwtManagerMock)
+	router, responseRecorder := prepareGin()
+	router.GET("/", sut.Get)
+
+	router.ServeHTTP(responseRecorder, prepareRequest(&OFFSET, &COUNT))
+
+	if responseRecorder.Code != 500 {
+		t.Error(responseRecorder.Code)
+	}
+	if responseRecorder.Body.Len() != 0 {
+		t.Error("Expected empty response")
+	}
+}
+
+func TestFileDataEndpointShouldReturnImageAsThumbnailWhenThereIsNoThumbnail(t *testing.T) {
+	expectedFileData := make([]endpoint.FileData, len(FILE_DATA_WITHOUT_THUMBNAIL))
+	copy(expectedFileData, FILE_DATA_WITHOUT_THUMBNAIL)
+	expectedFileData[0].Thumbnail = base64.StdEncoding.EncodeToString(FILE)
+	fileId, _ := strconv.ParseInt(expectedFileData[0].Id, 10, 64)
+
+	databaseMock := mock.NewDatabaseMock(t)
+	databaseMock.ExpectQuery(FILE_DATA_QUERY, fileDataToRequestArgs(FILE_DATA_WITHOUT_THUMBNAIL), []any{USER_ID, int64(15), int64(10)}, nil)
+	databaseMock.ExpectQuery("SELECT file FROM files WHERE id = $1", [][]any{{FILE}}, []any{fileId}, nil)
+	defer databaseMock.AssertAllExpectionsSatisfied()
+
+	jwtManagerMock := mock.NewJwtManagerMock(t)
+	jwtManagerMock.ExpectDecode(TOKEN_STRING, jwt.JwtPayload{
+		UserId: USER_ID,
+	}, nil)
+	defer jwtManagerMock.AssertAllExpectionsSatisfied()
+
+	sut := endpoint.NewFileDataEndpoint(&databaseMock, &jwtManagerMock)
+	router, responseRecorder := prepareGin()
+	router.GET("/", sut.Get)
+
+	router.ServeHTTP(responseRecorder, prepareRequest(&OFFSET, &COUNT))
+
+	if responseRecorder.Code != 200 {
+		t.Error(responseRecorder.Code)
+	}
+
+	var result []endpoint.FileData
+	err := json.Unmarshal(responseRecorder.Body.Bytes(), &result)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if !reflect.DeepEqual(result, expectedFileData) {
+		t.Error("Expected different file data")
+	}
+}
+
 func TestFileDataEndpointShouldReturn200AndFileData(t *testing.T) {
 	databaseMock := mock.NewDatabaseMock(t)
-	databaseMock.ExpectQuery(FILE_DATA_QUERY, fileDataToRequestArgs(FILE_DATA), []any{USER_ID, int64(15), int64(10)}, nil)
+	databaseMock.ExpectQuery(FILE_DATA_QUERY, fileDataToRequestArgs(FILE_DATA_WITH_THUMBNAIL), []any{USER_ID, int64(15), int64(10)}, nil)
 	defer databaseMock.AssertAllExpectionsSatisfied()
 
 	jwtManagerMock := mock.NewJwtManagerMock(t)
@@ -273,7 +391,7 @@ func TestFileDataEndpointShouldReturn200AndFileData(t *testing.T) {
 	if err != nil {
 		t.Error(err.Error())
 	}
-	if !reflect.DeepEqual(fileData, FILE_DATA) {
+	if !reflect.DeepEqual(fileData, FILE_DATA_WITH_THUMBNAIL) {
 		t.Error("Expected different file data")
 	}
 }
