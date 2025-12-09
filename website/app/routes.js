2cd18f2ca9d2ceba49dd6ca8ca61e963abf73f95
Narehate <matipietka@gmail.com>
2025-12-09 21:21:02 +0100

    [WEBSITE] Fixed white background flickering on  reloading

--- website/app/routes.js
+++ website/app/routes.js
@@ -1,11 +1,9 @@
 import { index, route, layout} from "@react-router/dev/routes";
 
 export default [
-    layout("routes/layout.jsx", [
         index("routes/index.jsx"),
         route("/register", "routes/register.jsx"),
         route("/login", "routes/login.jsx"),
         route("/gallery", "routes/gallery.jsx"),
         route("/error", "routes/error.jsx"),
-    ])
 ];
